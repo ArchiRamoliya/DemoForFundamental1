c03748aefeb4e3912574e88798e55c61c4a005a8
ArchiRamoliya <ramoliar@sheridancollege.ca>
2025-01-17 12:29:07 -0500

    fetch and merge

--- src/demoforgit/DemoForGit.java
+++ src/demoforgit/DemoForGit.java
@@ -13,6 +13,8 @@ public class DemoForGit {
     /**
      * @param args the command line arguments
      */
+    //show fetch
+    //show merge
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World");
